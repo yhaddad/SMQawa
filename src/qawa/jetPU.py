56283b2e3543012401b6178ccea8c3b6f3657800
israr100 <israr.i@northeastern.edu>
2025-06-05 10:23:28 +0200

    jetPU

--- src/qawa/jetPU.py
+++ src/qawa/jetPU.py
@@ -29,25 +29,45 @@ class jetPUScaleFactors:
 
 
     def getSF(self, jet, syst="nom"):
-        njets = ak.num(jet)
-        jets  = ak.flatten(jet)
-        jet_eta = np.array(jets.eta)
-        jet_pt  = np.array(jets.pt)
+        njets    = ak.num(jet)
+        jets     = ak.flatten(jet)
+        jet_eta  = np.array(jets.eta)
+        jet_pt   = np.array(jets.pt)
+        jet_puid = np.array(jets.puId)
         jet_sf = self.evaluator["PUJetID_eff"].evaluate(
             jet_eta,
             jet_pt,
             syst, self._wp
             )
-        jet_sf = ak.fill_none(jet_sf, 1.)
-        sf = ak.unflatten(jet_sf, njets)
-        sf = ak.prod(sf, axis=-1)
-        return sf
+        jet_eff = self.evaluator["PUJetID_eff"].evaluate(
+            jet_eta,
+            jet_pt,
+            'MCEff', self._wp
+            )
+        if   self._wp == 'M':
+            # tagged = (jet_puid>=6) | (jet_puid == 3)
+            tagged = np.where((jet_puid>=6) | (jet_puid == 3),True, False)
+        elif self._wp == 'T':
+            tagged = jet_puid>=7
+        elif self._wp == 'L':
+            tagged = jet_puid>=1
+        tagged_sf   = np.minimum(jet_sf*jet_eff, 1)/jet_eff
+        untagged_sf = np.maximum(1.- jet_sf*jet_eff,0)/(1-jet_eff)
+        tagged_sf   = ak.unflatten(tagged_sf  , njets)
+        untagged_sf = ak.unflatten(untagged_sf, njets)
+        
+        tagged      = ak.unflatten(tagged     , njets)
+        
+        tagged_sf   = ak.prod(tagged_sf[tagged]  , axis=-1)
+        untagged_sf = ak.prod(untagged_sf[~tagged], axis=-1)
+
+        return ak.fill_none(tagged_sf * untagged_sf, 1.)
+
     
 
 
     def append_jetPU_sf(self, jets: ak.Array, weights: Weights):
-        jets = jets[(jets.pt <= 50) & (np.abs(jets.eta) <= 5) & (jets.pt >= 20)]
-        
+        jets = jets[(jets.genJetIdx != -1) & (jets.pt <= 50) & (np.abs(jets.eta) <= 5) & (jets.pt >= 30)]
         sf_nom  = self.getSF(jets, 'nom')
         sf_up   = self.getSF(jets, 'up')
         sf_down = self.getSF(jets, 'down')
